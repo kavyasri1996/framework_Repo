2782b8a47d6187935d5bee97468440c8371f41af
Lenovo <Lenovo@DESKTOP-6R86IG7>
2024-05-17 14:53:29 +0530

    comitted

---
+++ SampleFramework/src/test/java/OrgTest/OrgTest.java
@@ -0,0 +1,19 @@
+package OrgTest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createorgTest()
+	{
+		System.out.println("execute createorgTest");
+		
+	}
+	@Test
+	public void modifyorgTest()
+	{
+		System.out.println("execute modifyorgTest");
+		
+	}
+
+}
